7d8c1c365ea4fb3da622f01bf9e44d018bc266eb
jakeby <jakeby@163.com>
2020-04-06 19:18:50 +0800

    更改banner切换动画

--- js/public.js
+++ js/public.js
@@ -41,12 +41,16 @@ $(function () {
 var swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
   effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  autoplay: {
+    delay: 4000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
   },
 });
