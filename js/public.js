be62673cd61e4962549b8fa5fb43866292276463
jakeby <jakeby@163.com>
2020-04-06 18:22:58 +0800

    添加title和banner

--- js/public.js
+++ js/public.js
@@ -36,3 +36,17 @@ $(function () {
     });
   })
 })
+
+//banner
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
